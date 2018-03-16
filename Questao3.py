1332b06b1c36b00fc58d787e90bfe81bfaa4d5be
DiogoTakayama <diogo.takayamas@gmail.com>
2018-03-16 17:30:23 -0300

    Add files via upload

---
+++ Questao3.py
@@ -0,0 +1,9 @@
+Python 3.6.5rc1 (v3.6.5rc1:f03c5148cf, Mar 14 2018, 03:12:11) [MSC v.1913 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/suporte/AppData/Local/Programs/Python/Python36/Questao3.py 
+Traceback (most recent call last):
+  File "C:/Users/suporte/AppData/Local/Programs/Python/Python36/Questao3.py", line 1, in <module>
+    a=int (input)
+TypeError: int() argument must be a string, a bytes-like object or a number, not 'builtin_function_or_method'
+>>> 
