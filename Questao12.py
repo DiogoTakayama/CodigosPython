a33422de64f76f297e7651dc99da24a67e8b888f
DiogoTakayama <diogo.takayamas@gmail.com>
2018-03-21 20:29:34 -0300

    Add files via upload

---
+++ Questao12.py
@@ -0,0 +1,8 @@
+'''
+Faça um programa que calcule e mostre o valor da área de um losango.
+Sabe-se que área do losango= diagonal maior*diagonal menor/2 .
+Seu programa deve receber os valores para as diagonais
+'''
+DiagonalMaior= float (input("Diagonal Maior: "))
+DiagonalMenor= float (input("Diagonal Menor: "))
+print (DiagonalMaior*DiagonalMenor/2)
---
+++ Questao13.py
@@ -0,0 +1,8 @@
+'''
+Faça um programa que receba o valor do salário-mínimo e o valor do salário
+de um funcionário,
+calcule e mostre a quantidade de salários-mínimos que esse funcionário recebe.
+'''
+SalarioMinimo = float (input("Salario Mnimo: "))
+Salario= float (input("Salario do Funcionario: "))
+print (Salario/SalarioMinimo)
