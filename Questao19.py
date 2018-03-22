c48fde8a57723249eeada08c6c374fb7857fcc63
DiogoTakayama <diogo.takayamas@gmail.com>
2018-03-21 23:49:04 -0300

    Add files via upload

---
+++ Questao19.py
@@ -0,0 +1,8 @@
+'''
+Faça um programa que receba uma temperatura em Celsius,
+calcule e mostre essa temperatura em Fahrenheit.
+Sabe-se que F= 9/5*C+32*
+
+'''
+Celsius=float (input("Celsius: "))
+print ("Farenheit: {:.4f}".format(9/5*Celsius+32))
